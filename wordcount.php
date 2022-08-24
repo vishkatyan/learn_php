3932ba59377eaa87c61e7cc2582326fb02214150
Vish katyan <82721870+vishkatyan@users.noreply.github.com>
2022-08-24 13:51:30 +0530

    Create wordcount.php

---
+++ wordcount.php
@@ -0,0 +1,5 @@
+<?php
+$name = "Vishakha Sharma";
+echo strlen($name)."<br>";   //String Function to find the length of the string.
+echo str_word_count($name);  //String Function to count no of words in a string
+?>
